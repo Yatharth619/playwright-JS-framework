8b4fb683e0168b35e3b2d5b2245970ad8757c795
Yatharth619 <eldrago24567@gmail.com>
2026-09-22 01:59:03 +0530

    yml file changes

--- tests/checkout.spec.js
+++ tests/checkout.spec.js
@@ -18,6 +18,7 @@ test("Successful checkout", async({checkoutpage}) =>
         await expect(checkoutpage.page).toHaveURL(/checkout/)
         await checkoutpage.proceedToCheckout()
         await checkoutpage.login(users.user2.email, users.user2.password)
+        await checkoutpage.page.screenshot({path: 'login-after-ci.png', fullPage: true})
         await expect(checkoutpage.loggedInMessage(users.user2.name)).toBeVisible({ timeout: 15000 })
         await checkoutpage.proceedPostLogin()
         await checkoutpage.addAddress(userDetails.user2.address)
